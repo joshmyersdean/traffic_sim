5f29594a5931492f7da80cb34c46676248628dba
Josh Myers-Dean <josh.myers-dean@colorado.edu>
2025-05-05 13:04:30 -0600

    remove POMDPModelTools dependency, add MCTS policy

--- julia/test.jl
+++ julia/test.jl
@@ -1,6 +1,7 @@
 using POMDPs
 using QuickPOMDPs
 using POMDPTools
+using MCTS
 using DataFrames
 
 include("model.jl")
@@ -22,6 +23,7 @@ function POMDPs.action(policy::MyDeterministicPolicy, s)
     end
 end
 
+println("Using deterministic policy...")
 policy = MyDeterministicPolicy()
 
 initial_state = IntersectionState(
@@ -32,6 +34,39 @@ initial_state = IntersectionState(
 hr = HistoryRecorder(max_steps=100)
 history = simulate(hr, m, policy, initial_state)
 # @show history
+
+function history_to_dataframe(history)
+    return DataFrame(
+        CarsState = [h[1].car_queue for h in history],
+        LightState = [h[1].light_state for h in history],
+        Action = [h[2] for h in history],
+        NextCarsState = [h[3].car_queue for h in history],
+        NextLightState = [h[3].light_state for h in history],
+        Reward = [h[4] for h in history],
+        TimeStep = [h[6] for h in history]
+    )
+end
+
+df = history_to_dataframe(history)
+show(df, allrows=true, allcols=true)
+
+println()
+
+total_reward = sum([h[4] for h in history])
+@show total_reward
+
+println("Using MCTS...")
+solver = MCTSSolver(n_iterations=10_000, depth=40, exploration_constant=5.0)
+planner = solve(solver, m)
+
+hr = HistoryRecorder(max_steps=100)
+
+initial_state = IntersectionState(
+    fill(5, 12), 
+    :off
+)
+
+history = simulate(hr, m, planner, initial_state)
 
 function history_to_dataframe(history)
     return DataFrame(
